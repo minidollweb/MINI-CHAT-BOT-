59f4a8a94c034675bceb4b3b44789c208f60e3f5
minidollweb <minidoll6t7@gmail.com>
2026-06-14 18:21:46 +0600

    Update Help.js

--- Script/commands/Help.js
+++ Script/commands/Help.js
@@ -143,7 +143,7 @@ module.exports.run = function ({ api, event, args, getText }) {
 ${msg}
 ┣━━━━━━━━━━━━━━━━┫
 ┃ ⚙ Prefix: ${prefix}
-┃ 🤖 Bot Name: ${global.config.BOTNAME || "𝐒𝐡𝐚𝐡𝐚𝐝𝐚𝐭 𝐂𝐡𝐚𝐭 𝐁𝐨𝐭"}
+┃ 🤖 Bot Name: ${global.config.BOTNAME || "MINI DOLL 2.0"}
 ┃ 👑 Owner: Picchi levi
 ╰━━━━━━━━━━━━━━━━╯`;
 
